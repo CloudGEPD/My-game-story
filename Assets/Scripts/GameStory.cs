8d9c26c72da47a4b9eb0fecba34bda7a7f2b4320
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-07-23 12:50:31 -0500

    Title, Scripts
    State scripts and calling them

---
+++ Assets/Scripts/GameStory.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStory : MonoBehaviour
+{
+    State states;
+    
+    void ManageNextStates()
+    {
+        states.DiffStates();
+
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class State : ScriptableObject
+{
+    [SerializeField] TextMeshProUGUI textmesh;
+    [TextArea(10,14)] [SerializeField] string storyText;
+    [SerializeField] State[] state;
+
+    public string StoryText()
+    {
+        return storyText;
+    }
+
+    public State[] DiffStates()
+    {
+        return state;
+    }
+
+}
