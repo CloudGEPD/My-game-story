a6f91b2e9a2bd3f0537d305a4aeb7fda2a4e967c
CloudGEPD <38668514+CloudGEPD@users.noreply.github.com>
2018-07-27 17:07:41 -0500

    Fixed 2nd button not working

---
+++ Assets/Scripts/GameStory.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameStory : MonoBehaviour
+{
+   [SerializeField] State startingState;
+   [SerializeField] TextMeshProUGUI textmesh;
+
+    State state;
+	
+
+	void Start ()
+    {
+        state = startingState;
+        textmesh.text = state.CoreStory(); 
+	}
+	
+	
+
+	void Update ()
+    {
+        GetNextStates();
+	}
+
+    void GetNextStates()
+    {
+        var changeStates = state.ManageNextStates();
+        if(Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            state = changeStates[0];
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            state = changeStates[1];
+        }
+
+        textmesh.text = state.CoreStory();
+    }
+}
---
+++ Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "State")]
+
+public class State : ScriptableObject
+{
+   [TextArea(14,14)] public string coreStory;
+   public State[] changeStates;
+
+    public string CoreStory()
+    {
+        return coreStory;
+    }
+
+    public State[] ManageNextStates()
+   {
+       return changeStates;
+   }
+
+}
